5bf83e3f6aeef77a736a16df8c46d6e77e490db8
Fernanda Dellajustina <fernandadellajustina@gmail.com>
2016-10-26 09:04:17 -0200

    Aprimoramento da função apli_qui() na qual agora temos controle do valor da dose máxima do quimioterápico phiMax. Também foi atualizado a biblioteca bts-vars0.47.h, para automatizar a escolha de parâmetros de controle que são passadas por pré compilação para o código.

---
+++ mestrado/modelo-baseado-sanjuan/v0.4/bts-util-0.47.h
@@ -0,0 +1,82 @@
+// #############################################################################
+// Make the header of the archives with variables of the system
+void make_header (FILE * o) {
+  fprintf (o, "# GROWTH TUMOR SIMULATION bts-%s.c (%s)\n", vers_id, date_id);
+  fprintf (o, "# 2016 (C) %s\n", auth1_id);
+  fprintf (o, "#          %s\n", dept1_id);
+  fprintf (o, "#          %s\n", host1_id);
+  fprintf (o, "#\n");
+  fprintf (o, "#        NN = %20d\n", NN);
+  fprintf (o, "#\n");
+  fprintf (o, "#       a12 = %10.5e\n", a12);
+  fprintf (o, "#       a13 = %10.5e\n", a13);
+  fprintf (o, "#       a21 = %10.5e\n", a21);
+  fprintf (o, "#       a31 = %10.5e\n", a31);
+  fprintf (o, "#        r2 = %10.5e\n", r2);
+  fprintf (o, "#        r3 = %10.5e\n", r3);
+  fprintf (o, "#        d3 = %10.5e\n", d3);
+  fprintf (o, "#        k3 = %10.5e\n", k3);
+  fprintf (o, "#\n");
+  fprintf (o, "#      nphi = %5d\n", nphi);
+  fprintf (o, "#      ntd2 = %5d\n", ntd2);
+  fprintf (o, "#   phi_min = %10.5e\n", phi_min);
+  fprintf (o, "#   phi_max = %10.5e\n", phi_max);
+  fprintf (o, "#   td2_min = %10.5e\n", td2_min);
+  fprintf (o, "#   td2_max = %10.5e\n", td2_max);
+  fprintf (o, "#\n");
+  fprintf (o, "#    phiMax = %10.5e\n", phiMax);
+  fprintf (o, "#    Gamma  = %10.5e\n", Gamma);
+  fprintf (o, "#\n");
+  fprintf (o, "#       td1 = %10.5e\n", td1);
+  fprintf (o, "#       td2 = %10.5e\n", td2);
+  fprintf (o, "#       eps = %10.5e\n", eps);
+  fprintf (o, "#      epst = %10.5e\n", epst);
+  fprintf (o, "#      ntx0 = %d\n", ntx0);
+  fprintf (o, "#   freqQui = %d\n", freqQui);
+  fprintf (o, "#\n");
+  fprintf (o, "#      tmax = %10.5e\n", tmax);
+  fprintf (o, "#         h = %10.5e\n", h);
+  fprintf (o, "#       tol = %10.5e\n", tol);
+  fprintf (o, "#\n");
+}                                                      // end for function make_header(FILE *o)
+
+// DEQUANT
+double dequant (int i, int min, int max, double xmin, double xmax) {
+  return (xmin + (xmax - xmin) * (double) (i - min) / (double) (max - min));
+}
+
+void apli_qui(double phi0, double phiMax, double t){
+  if(td1 < 1.0e-5) phi = phi0;
+  else{
+    if(t1 < td1){
+      t1 += h;
+
+      if(dt1 < td1_3){
+        phi=exp(Gamma*dt1)-1.0e0;
+        dt1+=h;
+      }
+      else{
+        if((dt1 > td1_3) && (dt1 < 2.0e0*td1_3)){
+          dt1+=h;
+        }
+        else{
+          if((dt1 > 2.0e0*td1_3) && (dt1 < 3.0e0*td1_3)){
+            phi=phiMax*exp(-Gamma*fabs(dt1-2.0e0*td1_3))-1.0e0;
+            dt1+=h;
+          }
+        }
+      }
+
+      if(fabs(t1 - td1) < eps) t2 = 0.0e0;
+    }
+    else{
+      t2 += h;
+      phi = 0.0e0;
+      if(fabs(t2 - td2) < eps){
+         t1 = 0.0e0;
+         dt1 = 0.0e0;
+      }
+    }
+  }
+
+}
---
+++ mestrado/modelo-baseado-sanjuan/v0.4/bts-v0.47.c
@@ -0,0 +1,135 @@
+/*********************************************************************
+ FERNANDA J. DELLAJUSTINA | EMAIL: fernandadellajustina@gmail.com
+      UFPR, DEPARTAMENTO DE FÍSICA, CURITIBA-PR, BRASIL
+
+ versao 0.4               | 20.09.2016 13:31:36
+ Otimização da aplicação do quimioterápico.
+
+ versao 0.43              | 10.10.2016 13:31:36
+ testando pontos do espaço de parâmetros.
+
+ versao 0.45              | 18.10.2016 07:57:04
+ Após x ir a zero com a aplicação do quimioterápico para ntx0 tempos,
+ não se aplica mais quimio e observa-se o comportamento do sistema.
+
+ versao 0.46              | 23.10.2016 15:49:06
+ Melhoramento da aplicação do quimioterápico, com uma função exponencial
+ até um certo limite
+
+ versao 0.47              | 25.10.2016 14:39:06
+ Melhoramento da aplicação do quimioterápico, com controle do phiMax.
+
+ Sistema de equações:
+ dx/dt = x*(1-x) - a12*x*y - a13*x*z - a14*x*q;
+ dy/dt = r2*y*(1-y) - a21*x*y - a2*y*q;
+ dz/dt = r3*z*x/(x + k3) - a31*z*x - d3*z - a32*z*q;
+ dq/dt = phi - d4*q;
+ ********************************************************************/
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<time.h>
+#include<omp.h>           // OpenMP include file
+
+const char  vers_id[8]    = "v0.47";
+const char  date_id[16]   = "25/10/2016";
+const char  auth1_id[128] = "Fernanda Jaiara Dellajustina, e-mail: fernandadellajustina@gmail.com";
+const char  dept1_id[128] = "Departamaneto de Física";
+const char  host1_id[128] = "UFPR, Curitiba-PR, Brasil";
+
+#include "bts-vars-0.47.h"                         // parametros/variaveis
+#include "bts-util-0.47.h"                         // bibliotecas
+#include "bts-ydot-0.4.h"                          // derivs function
+#include "/home/fernanda/util/dverk.c"             // integrador
+
+double      y[NN];
+
+//  ---------------------------------------------------------------------
+int main () {
+  int         ind, loop, i, j, timex0, ext_tumor;
+  double      phi0;
+  double      t, tout, w[NN * 9], c[24], sumx;
+
+  char file[256];
+  FILE *a;
+  sprintf(file, "bts-%s.dat", vers_id);
+  if ((a = fopen(file, "w")) == NULL) return 1;
+
+  make_header (stdout);
+  make_header (a);
+
+  freqQui = 1;               // frequencia de aplicação do quimio
+  ntx0    = 100.0e0 / h;     // numero de tempo seguidos com x=0
+  eps     = h;               // contagem do período
+  epst    = 1.0e-2;          // extinção do tumor
+
+  #pragma omp parallel private(i,j)
+  {
+    for (i = 0; i < nphi; i++){
+      phi0 = dequant(i, 0, nphi, phi_min, phi_max);
+      printf("phi = %6.2f i = %d \n", phi0, i);                                  // print na tela, controle!
+
+      #pragma omp single
+      {
+        for (j = 0; j <  ntd2; j++){
+          td2 = dequant(j,0,ntd2,td2_min,td2_max);      // tempo de aplicação do quimio
+          td1 = 3.0e0 * td2;                            // tempo sem aplicação do quimio
+
+          y[0] = 0.2e0;                                 // x
+          y[1] = 0.9e0;                                 // y
+          y[2] = 0.1e0;                                 // z
+          y[3] = phi_max;                               // q
+
+          t    = 0.0e0;
+          ind  = 1;
+          loop = 0;
+          iT   = 1;
+          nT   = td2;                                   // começa com aplicação
+          timex0 = 0;                                   // contagem de tempo em que x=0
+          ext_tumor = 0;                                // extinção do tumor? (FLAG)
+          sumx   = 0.0e0;                               // média de x
+          dt1    = 0.0e0;
+          t1     = 0.0e0;
+          t2     = 0.0e0;
+          td1_3  = td1/3.0e0;
+          phiMax = 10.0e0;                              // phi máximo
+          Gamma  = log(phiMax)/td1_3;
+
+          // órbita
+          while (t <= tmax) {
+             if(loop % 1 == 0) fprintf(a,"%5.2f %12.6f %12.6f %12.6f %12.6f %12.6f\n", t, y[0], y[1], y[2], y[3], phi);
+
+              if(ext_tumor == 0) {                      // tumor foi extinto?
+                apli_qui(phi0, phiMax, t);    // função que aplica o quimioterápico
+              }
+
+              tout = t + h;
+              dverk (&nn, &t, y, &tout, &tol, &ind, c, &nn, w);
+              if (ind != 3) printf ("\nDVERK ERROR, IND=%d.\n\n", ind);
+              loop++;
+
+             if(ext_tumor == 0) {
+                if(fabs(y[0]) <= epst){
+                  timex0++;
+                  sumx+=y[1];
+                  if(timex0 == ntx0){                   // x=0 para ntx0 tempo?
+                    ext_tumor = 1;
+                    phi = 0.0e0;                        // sem câncer por ntx0 tempos? então paramos de aplicar quimioterápico.
+                    //y[3] = 0.0e0;
+                    //break;
+                  }
+              }
+              else {
+                timex0 = 0;
+                sumx   = 0.0e0;
+              }
+             }
+          }  // end loop while
+        }   //end loop td2
+      }    // and single region
+    }     //end loop phi0
+  }
+
+ fclose (a);
+ return 0;
+}
